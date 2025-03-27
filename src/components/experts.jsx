8ec553590eb37ca8d52d192bbf70718afc1bb479
Leeobenga <111017790+Leeobenga@users.noreply.github.com>
2025-03-27 03:13:05 +0300

    Update experts.jsx

--- src/components/experts.jsx
+++ src/components/experts.jsx
@@ -1,3 +1,9 @@
+import woman from "/src/assets/woman-6553477.jpg"
+import facebook from "/src/assets/icons8-facebook-20.png"
+import x from "/src/assets/icons8-x-20.png"
+import linkedIn from "/src/assets/icons8-linkedin-20.png"
+import instagram from "/src/assets/icons8-instagram-20.png"
+import business from "/src/assets/business-695489.jpg"
 
 const Expert = () => {
     return (
@@ -12,46 +18,46 @@ const Expert = () => {
             <section className="expert-cards">
                 <div className="expert">
                     <div className="expert-image">
-                        <img src="/src/assets/woman-6553477.jpg" alt="CEO" className="expert-img" />
+                        <img src={woman} alt="CEO" className="expert-img" />
                     </div>
                     <div className="expert-details">
                         <h2>Tracy Obirika <span className="title">/ FOUNDER</span></h2>
                         <p>A naturalist is a person who studies the natural world, including plants, animals, and their environments.…</p>
                         <div className="expert-socials-container">
                             <div className="expert-social">
-                                <img src="/src/assets/icons8-facebook-20.png" alt="facebook" className="expert-social-logo" />
+                                <img src={facebook} alt="facebook" className="expert-social-logo" />
                             </div>
                             <div className="expert-social">
-                                <img src="/src/assets/icons8-x-20.png" alt="X" className="expert-social-logo" />
+                                <img src={x} alt="X" className="expert-social-logo" />
                             </div>
                             <div className="expert-social">
-                                <img src="/src/assets/icons8-linkedin-20.png" alt="linkedin" className="expert-social-logo" />
+                                <img src={linkedIn} alt="linkedin" className="expert-social-logo" />
                             </div>
                             <div className="expert-social">
-                                <img src="/src/assets/icons8-instagram-20.png" alt="instagram" className="expert-social-logo" />
+                                <img src={instagram} alt="instagram" className="expert-social-logo" />
                             </div>
                         </div>                    
                     </div>
                 </div>
                 <div className="expert2">
                     <div className="expert-image">
-                        <img src="/src/assets/business-695489.jpg" alt="CEO" className="expert-img" />
+                        <img src={business} alt="CEO" className="expert-img" />
                     </div>
                     <div className="expert-details">
                         <h2>Rio Ferdinand <span className="title">/ ENGINEER</span></h2>
                         <p>A naturalist is a person who studies the natural world, including plants, animals, and their environments.…</p>
                         <div className="expert-socials-container">
                             <div className="expert-social">
-                                <img src="/src/assets/icons8-facebook-20.png" alt="facebook" className="expert-social-logo" />
+                                <img src={facebook} alt="facebook" className="expert-social-logo" />
                             </div>
                             <div className="expert-social">
-                                <img src="/src/assets/icons8-x-20.png" alt="X" className="expert-social-logo" />
+                                <img src={x} alt="X" className="expert-social-logo" />
                             </div>
                             <div className="expert-social">
-                                <img src="/src/assets/icons8-linkedin-20.png" alt="linkedin" className="expert-social-logo" />
+                                <img src={linkedIn} alt="linkedin" className="expert-social-logo" />
                             </div>
                             <div className="expert-social">
-                                <img src="/src/assets/icons8-instagram-20.png" alt="instagram" className="expert-social-logo" />
+                                <img src={instagram} alt="instagram" className="expert-social-logo" />
                             </div>
                         </div>                    
                     </div>
