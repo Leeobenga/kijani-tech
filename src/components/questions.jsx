aa7d9ec02606b56c0f4e086a9939af4dfbb7367a
Leeobenga <111017790+Leeobenga@users.noreply.github.com>
2025-03-28 05:52:33 +0300

    assets and questions update

--- src/components/questions.jsx
+++ src/components/questions.jsx
@@ -30,14 +30,14 @@ const Questions = () => {
                 <h1>Keep the scene green by taking the lead</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quod rerum illo nesciunt dolorem ipsa earum debitis nisi doloremque voluptas, vel est, et sunt delectus, eaque modi magni laboriosam corporis!</p>
                 <div className="questions">
-                    <p className="question">What is the purpose of the company</p>
+                    <p className="question">What is the purpose of the company?</p>
                     <button onClick={toggleQuestionOne}><span className="add">{showQuestionOne ? "-": "+"}</span></button>
                     { showQuestionOne && <div className="questions-answer">
                         <p>Mkulima Green Solutions is committed to transforming Kenya's agricultural sector by providing affordable and efficient solar-powered irrigation systems. Our goal is to help farmers overcome water scarcity, increase crop yields and adopt sustainable farming practices for long-term food security</p>
                     </div>}
                 </div>
                 <div className="questions">
-                    <p className="question">What makes your company unique?</p>
+                    <p className="question">What makes our company unique?</p>
                     <button onClick={toggleQuestionTwo}><span className="add">{showQuestionTwo ? "-": "+"}</span></button>
                     { showQuestionTwo && <div className="questions-answer">
                         <p>Farmer-First Innovation: Designed with input from local farmers to address their specific irrigation challenges</p>
