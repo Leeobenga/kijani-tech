601efa82afb2bff0bc65d370f91332da5c96bc42
Leeobenga <111017790+Leeobenga@users.noreply.github.com>
2025-03-29 10:34:48 +0300

    more updates on donation.jsx and css

--- src/pages/donation.jsx
+++ src/pages/donation.jsx
@@ -11,12 +11,12 @@ const Donation = ({articles}) => {
     const [filteredArticles, setFilteredArticles] = useState([]);
     console.log(filteredArticles)
     const [searchParams] = useSearchParams();
-    console.log(searchParams)
+    let currentIndex; 
 
     useEffect(() => {
         if (articles) {
             const currentArticleId = parseInt(searchParams.get("articleId"), 10);
-            console.log(currentArticleId);
+            console.log(currentArticleId);            
 
             if (!currentArticleId) {
                 console.warn("No article ID found in query params");
@@ -29,7 +29,21 @@ const Donation = ({articles}) => {
         }
     }, [articles, searchParams]);
 
+    const getCurrentArticle = () => {
+        const currentArticleId = parseInt(searchParams.get("articleId"), 10);
+        articles.map((article, index) => {
+            if (article.id === currentArticleId)  {
+                currentIndex = index
+            }          
+    });        
+        return currentIndex;
+    }  
+    getCurrentArticle();
    
+    const riseWidth = (index) => {
+        return (articles[index].rise / articles[index].goal) * 100
+    };
+    console.log(currentIndex)
     return (
         <>
          <PageNavbar />
@@ -42,7 +56,7 @@ const Donation = ({articles}) => {
                         <div key={article.id} className="donate-article">
                             <img src={article.img} alt="" className="donation-img" />
                             <div className="description">
-                                <h3 className="">{article.header}</h3>
+                                <h4 className="">{article.header}</h4>
                                 <p>{article.description}</p>
                                 <p className="article-goal">GOAL: ${article.goal}</p>
                             </div>                            
@@ -51,7 +65,34 @@ const Donation = ({articles}) => {
                 </div>
             </div>
             <div className="donate-status-container">
-                
+                <div className="donation-status">
+                    <div className="donation-progress" style={{width:`${riseWidth(currentIndex)}%` }}></div>
+                    <span className="donation-span"><span>Raise: ${articles[currentIndex].rise}</span><span>GOAL: ${articles[currentIndex].goal}</span></span>
+                </div>
+            </div>
+            <div className="donation-description">
+                <div className="donation-details">
+                    <h1>{articles[currentIndex].header}</h1>
+                    <p>
+                        Training farmers to optimize production using solar pumps enhances efficiency, 
+                        reduces <br /> costs, and promotes sustainability. Solar-powered irrigation provides a reliable
+                        water <br /> source, increasing yields while reducing dependency on expensive and polluting 
+                        fossil fuels. 
+                    </p> 
+                    <div className="donation-contact"></div> 
+                    <div className="donation">
+                        <span>Your Donation:</span>
+                        <span>$</span>
+                    </div>  
+                    <div className="donation-buttons">
+                        <button className="donation-button">$10</button>
+                        <button className="donation-button">$20</button>
+                        <button className="donation-button">$50</button>
+                        <button className="donation-button">$100</button>
+                        <button className="donation-button">$1000</button>
+                    </div> 
+                </div>  
+                 
             </div>
          </div>
         </>
