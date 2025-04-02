2c472bee259172fd2204b3824b56798dcf7467a9
Leeobenga <111017790+Leeobenga@users.noreply.github.com>
2025-04-02 10:42:43 +0300

    moved donation.css to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ import training from "/src/assets/train-farmer.avif"
 import solarPump from "/src/assets/solar-pump.png"
 import equipFarmer from "/src/assets/equipFarmer.jpg"
 import "./App.css"
+import "./components/Donation.css"
 
 
 
